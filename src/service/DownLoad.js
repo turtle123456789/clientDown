4a7aeca8a42443018473d20b20082ea70debdfd9
turtle123456789 <90664619+turtle123456789@users.noreply.github.com>
2024-07-21 18:07:59 +0700

    s

--- src/service/DownLoad.js
+++ src/service/DownLoad.js
@@ -1,16 +1,8 @@
 import axios from "axios"
 import fileDownload from 'js-file-download';
 export const axiosJWT = axios.create()
-export const getAll = async () =>{
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/testDown/getAll`)
-    return res.data
-}
-export const uploadFile = async (data) =>{
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/testDown/update`,data)
-    return res.data
-}
 export const downFile = async (data) =>{
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/testDown/down`,{
+    const res = await axios.get(`http://localhost:3001/api/testDown/down`,{
         params:{data},
         responseType: 'blob',
     }
